64a17c3ac5234ac562faf4badd1dbe81265e4d0c
Pairon03 <cauamoura09@gmail.com>
2024-08-23 20:00:31 -0300

    Update Aula3bigdata.py

--- Aula3bigdata.py
+++ Aula3bigdata.py
@@ -1,7 +1,10 @@
 import pandas as pd #importa a biblioteca pandas
-import numpy as np
-import matplotlib.pyplot as plt 
+import numpy as np #importa a biblioteca numpy
+import matplotlib.pyplot as plt #importa a biblioteca matplotlib
 
-Dados_x= np.linspace(0,10,10)
-Dados_y = Dados_x * Dados_x
+Dados_x= np.linspace(0,10,10) # gera dados em x de 10
+# em 10 valores
+Dados_y = Dados_x * Dados_x # multiplica os dados de x 
+# e armazena em y
 
+plt.figure(figsize=(10,8),facecolor='gray') #Cria uma figura
